94a1106358bbd2fd587fe2009f57eff44e2dc844
Sidiq <sidimawan@gmail.com>
2025-09-18 20:26:08 +0700

    Update sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ export default {
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./node_modules/flowbite/**/*.js", // <-- Tambahkan ini
+        "./resources/views//*.blade.php",
+        "./node_modules/flowbite//*.js", // <-- Tambahkan ini
     ],
 
     theme: {
