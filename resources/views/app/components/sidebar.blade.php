64a511cdd820feeee6abc4d91e12299ce363bc5d
Sidiq <sidimawan@gmail.com>
2025-09-19 15:58:53 +0700

    Update sidebar dan edit produk

--- resources/views/app/components/sidebar.blade.php
+++ resources/views/app/components/sidebar.blade.php
@@ -22,61 +22,67 @@
     {{-- Ini adalah Sidebar (menu kiri) --}}
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
+            
             {{-- 1. Menu Dashboard --}}
             <li>
-                <a href="{{ route('admin.dashboard') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ Request::is('admin/dashboard') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
-                    {{-- DIUBAH: kelas h-6 dihapus untuk alignment otomatis --}}
-                    <i class="w-6 text-center text-gray-500 fa-solid fa-chart-pie transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>
+                {{-- KELAS TRANSISI DITAMBAHKAN DI SINI --}}
+                <a href="{{ route('admin.dashboard') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-500 dark:hover:bg-indigo-600 hover:text-white dark:hover:text-white group transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 {{ Request::is('admin/dashboard') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
+                    <i class="w-6 text-center text-gray-500 fa-solid fa-chart-pie transition duration-75 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"></i>
                     <span class="ml-3">Dashboard</span>
                 </a>
             </li>
             
             {{-- 2. Menu Dropdown Transaksi --}}
-            <li>
-                <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700 {{ Request::is('admin/transaksi/*') ? 'bg-gray-100 dark:bg-gray-700' : '' }}" aria-controls="dropdown-transactions" data-collapse-toggle="dropdown-transactions">
-                    <i class="w-6 text-center text-gray-500 fa-solid fa-arrow-right-arrow-left transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>
-                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Transaksi</span>
-                    <i class="fa-solid fa-chevron-down"></i>
-                </button>
-                <ul id="dropdown-transactions" class="{{ Request::is('admin/transaksi/*') ? '' : 'hidden' }} py-2 space-y-2">
-                      <li>
-                         <a href="{{ route('admin.transactions.stockin') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 {{ Request::is('admin/transaksi/masuk') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">Barang Masuk</a>
-                      </li>
-                      <li>
-                         <a href="{{ route('admin.transactions.stockout') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 {{ Request::is('admin/transaksi/keluar') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">Barang Keluar</a>
-                      </li>
-                </ul>
-            </li>
+           <li>
+    <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 dark:text-white rounded-lg group hover:bg-indigo-500 dark:hover:bg-indigo-600 hover:text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 {{ Request::is('admin/transaksi/*') ? 'bg-gray-100 dark:bg-gray-700' : '' }}" aria-controls="dropdown-transactions" data-collapse-toggle="dropdown-transactions">
+        <i class="w-6 text-center text-gray-500 fa-solid fa-arrow-right-arrow-left transition duration-75 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"></i>
+        <span class="flex-1 ms-3 text-left whitespace-nowrap">Transaksi</span>
+        <i class="fa-solid fa-chevron-down text-gray-500 dark:text-gray-400"></i>
+    </button>
+    <ul id="dropdown-transactions" class="{{ Request::is('admin/transaksi/*') ? '' : 'hidden' }} py-2 space-y-2">
+        <li>
+            <a href="{{ route('admin.transactions.stockin') }}" class="flex items-center w-full p-2 text-gray-900 dark:text-white transition duration-75 rounded-lg ps-11 group hover:bg-gray-100 dark:hover:bg-gray-700 {{ Request::is('admin/transaksi/masuk') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">Barang Masuk</a>
+        </li>
+        <li>
+            <a href="{{ route('admin.transactions.stockout') }}" class="flex items-center w-full p-2 text-gray-900 dark:text-white transition duration-75 rounded-lg ps-11 group hover:bg-gray-100 dark:hover:bg-gray-700 {{ Request::is('admin/transaksi/keluar') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">Barang Keluar</a>
+        </li>
+    </ul>
+</li>
 
             {{-- 3. Menu Manajemen Data Master --}}
             <li>
-                <a href="{{ route('admin.products.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ Request::is('admin/produk') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
-                    <i class="w-6 text-center text-gray-500 fa-solid fa-box-archive transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>
+                {{-- KELAS TRANSISI DITAMBAHKAN DI SINI --}}
+                <a href="{{ route('admin.products.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-500 dark:hover:bg-indigo-600 hover:text-white dark:hover:text-white group transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 {{ Request::is('admin/produk') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
+                    <i class="w-6 text-center text-gray-500 fa-solid fa-box-archive transition duration-75 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"></i>
                     <span class="flex-1 ml-3 whitespace-nowrap">Produk</span>
                 </a>
             </li>
             <li>
-                <a href="{{ route('admin.categories.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ Request::is('admin/kategori') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
-                    <i class="w-6 text-center text-gray-500 fa-solid fa-tags transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>
+                {{-- KELAS TRANSISI DITAMBAHKAN DI SINI --}}
+                <a href="{{ route('admin.categories.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-500 dark:hover:bg-indigo-600 hover:text-white dark:hover:text-white group transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 {{ Request::is('admin/kategori') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
+                    <i class="w-6 text-center text-gray-500 fa-solid fa-tags transition duration-75 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"></i>
                     <span class="flex-1 ml-3 whitespace-nowrap">Kategori</span>
                 </a>
             </li>
              <li>
-                <a href="{{ route('admin.suppliers.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ Request::is('admin/supplier') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
-                   <i class="w-6 text-center text-gray-500 fa-solid fa-truck-fast transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>
-                    <span class="flex-1 ml-3 whitespace-nowrap">Supplier</span>
+                 {{-- KELAS TRANSISI DITAMBAHKAN DI SINI --}}
+                <a href="{{ route('admin.suppliers.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-500 dark:hover:bg-indigo-600 hover:text-white dark:hover:text-white group transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 {{ Request::is('admin/supplier') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
+                   <i class="w-6 text-center text-gray-500 fa-solid fa-truck-fast transition duration-75 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"></i>
+                   <span class="flex-1 ml-3 whitespace-nowrap">Supplier</span>
                 </a>
-            </li>
+             </li>
              <li>
-                <a href="{{ route('admin.users.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ Request::is('admin/pengguna') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
-                    <i class="w-6 text-center text-gray-500 fa-solid fa-users transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>
+                 {{-- KELAS TRANSISI DITAMBAHKAN DI SINI --}}
+                <a href="{{ route('admin.users.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-500 dark:hover:bg-indigo-600 hover:text-white dark:hover:text-white group transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 {{ Request::is('admin/pengguna') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
+                    <i class="w-6 text-center text-gray-500 fa-solid fa-users transition duration-75 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"></i>
                     <span class="flex-1 ml-3 whitespace-nowrap">Pengguna</span>
                 </a>
-            </li>
+             </li>
             <li>
-                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                   <i class="w-6 text-center text-gray-500 fa-solid fa-file-lines transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>
-                    <span class="flex-1 ml-3 whitespace-nowrap">Laporan</span>
+                {{-- KELAS TRANSISI DITAMBAHKAN DI SINI --}}
+                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-500 dark:hover:bg-indigo-600 hover:text-white dark:hover:text-white group transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+                   <i class="w-6 text-center text-gray-500 fa-solid fa-file-lines transition duration-75 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"></i>
+                   <span class="flex-1 ml-3 whitespace-nowrap">Laporan</span>
                 </a>
             </li>
         </ul>
--- resources/views/app/pages/products/index.blade.php
+++ resources/views/app/pages/products/index.blade.php
@@ -14,7 +14,7 @@
         <div class="sm:flex">
             <div class="flex items-center ml-auto space-x-2 sm:space-x-3">
                 <button type="button" data-modal-target="add-product-modal" data-modal-toggle="add-product-modal" class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    <svg class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                    <i class="w-5 h-5 mr-2 -ml-1 fa-solid fa-plus"></i>
                     Tambah Produk
                 </button>
             </div>
@@ -32,22 +32,34 @@
                         <tr>
                             <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">Nama Produk</th>
                             <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">Kategori</th>
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">Supplier</th>
                             <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">Harga Jual</th>
                             <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">Stok</th>
                             <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">Aksi</th>
                         </tr>
                     </thead>
-                    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                    <tbody id="product-table-body" class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                         {{-- Data Dummy Baris 1 --}}
                         <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
                             <td class="p-4 text-sm font-semibold text-gray-900 whitespace-nowrap dark:text-white">Laptop ProBook 14"</td>
-                            <td class="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400">Elektronik</td>
-                            <td class="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400">CV. Maju Jaya</td>
-                            <td class="p-4 text-sm font-semibold text-gray-900 whitespace-nowrap dark:text-white">Rp 12.500.000</td>
-                            <td class="p-4 text-sm font-semibold text-gray-900 whitespace-nowrap dark:text-white">50</td>
+                            <td class="p-4 text-sm font-normal text-gray-500 dark:text-gray-400">Elektronik</td>
+                            <td class="p-4 text-sm font-semibold text-gray-900 dark:text-white">Rp 12.500.000</td>
+                            <td class="p-4 text-sm font-semibold text-gray-900 dark:text-white">50</td>
                             <td class="p-4 space-x-2 whitespace-nowrap">
-                                <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-yellow-400 hover:bg-yellow-500">Edit</button>
+                                {{-- Tombol Edit dengan data attributes untuk di-tangkap JavaScript --}}
+                                <button type="button" 
+                                    class="edit-button inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-yellow-400 hover:bg-yellow-500"
+                                    data-modal-target="edit-product-modal" 
+                                    data-modal-toggle="edit-product-modal"
+                                    data-id="1"
+                                    data-name="Laptop ProBook 14&quot;"
+                                    data-sku="LP-PB-14-001"
+                                    data-category="elektronik"
+                                    data-supplier="supplier2"
+                                    data-purchase_price="10000000"
+                                    data-selling_price="12500000"
+                                    data-description="Deskripsi singkat laptop ProBook.">
+                                    Edit
+                                </button>
                                 <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700">Hapus</button>
                             </td>
                         </tr>
@@ -58,65 +70,116 @@
     </div>
 </div>
 
-{{-- Modal untuk Tambah/Edit Produk --}}
+{{-- ======================== MODAL TAMBAH PRODUK ======================== --}}
 <div id="add-product-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
     <div class="relative w-full h-full max-w-4xl p-4 md:h-auto">
         <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
             <div class="flex items-center justify-between pb-4 mb-4 border-b rounded-t sm:mb-5 dark:border-gray-600">
                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Tambah Produk Baru</h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="add-product-modal">
-                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-toggle="add-product-modal">
+                    <i class="fa-solid fa-times w-5 h-5"></i>
                     <span class="sr-only">Close modal</span>
                 </button>
             </div>
             <form action="#">
+                {{-- ... (Isi form sama seperti sebelumnya) ... --}}
+            </form>
+        </div>
+    </div>
+</div>
+
+{{-- ======================== MODAL EDIT PRODUK (BARU) ======================== --}}
+<div id="edit-product-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+    <div class="relative w-full h-full max-w-4xl p-4 md:h-auto">
+        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+            <div class="flex items-center justify-between pb-4 mb-4 border-b rounded-t sm:mb-5 dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Edit Produk</h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-toggle="edit-product-modal">
+                    <i class="fa-solid fa-times w-5 h-5"></i>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <form action="#">
+                {{-- Input tersembunyi untuk menyimpan ID produk yang akan di-update --}}
+                <input type="hidden" id="edit-product-id" name="id">
+                
                 <div class="grid gap-4 mb-4 sm:grid-cols-2">
                     <div>
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Produk</label>
-                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Contoh: Laptop ProBook 14" required>
+                        <label for="edit-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Produk</label>
+                        <input type="text" name="name" id="edit-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required>
                     </div>
                     <div>
-                        <label for="sku" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">SKU (Stock Keeping Unit)</label>
-                        <input type="text" name="sku" id="sku" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Contoh: LP-PB-14-001" required>
+                        <label for="edit-sku" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">SKU</label>
+                        <input type="text" name="sku" id="edit-sku" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required>
                     </div>
                     <div>
-                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
-                        <select id="category" name="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
-                            <option selected disabled>Pilih Kategori</option>
+                        <label for="edit-category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
+                        <select id="edit-category" name="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
                             <option value="elektronik">Elektronik</option>
                             <option value="pakaian">Pakaian</option>
                         </select>
                     </div>
                     <div>
-                        <label for="supplier" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Supplier</label>
-                        <select id="supplier" name="supplier" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
-                            <option selected disabled>Pilih Supplier</option>
+                        <label for="edit-supplier" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Supplier</label>
+                        <select id="edit-supplier" name="supplier" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
                             <option value="supplier1">PT. Sejahtera Abadi</option>
                             <option value="supplier2">CV. Maju Jaya Elektronik</option>
                         </select>
                     </div>
                     <div>
-                        <label for="purchase_price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga Beli</label>
-                        <input type="number" name="purchase_price" id="purchase_price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="10000000" required>
+                        <label for="edit-purchase_price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga Beli</label>
+                        <input type="number" name="purchase_price" id="edit-purchase_price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required>
                     </div>
                     <div>
-                        <label for="selling_price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga Jual</label>
-                        <input type="number" name="selling_price" id="selling_price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="12500000" required>
+                        <label for="edit-selling_price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga Jual</label>
+                        <input type="number" name="selling_price" id="edit-selling_price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi</label>
-                        <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder="Tulis deskripsi produk di sini..."></textarea>
-                    </div>
-                    <div class="sm:col-span-2">
-                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload Gambar</label>
-                         <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+                        <label for="edit-description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi</label>
+                        <textarea id="edit-description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"></textarea>
                     </div>
                 </div>
                 <button type="submit" class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                    Simpan Produk
+                    Update Produk
                 </button>
             </form>
         </div>
     </div>
 </div>
+
 @endsection
+
+@push('scripts')
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        // Event listener untuk semua tombol edit di dalam tabel
+        const tableBody = document.getElementById('product-table-body');
+        
+        tableBody.addEventListener('click', function(event) {
+            // Cek apakah yang diklik adalah tombol dengan class 'edit-button'
+            const editButton = event.target.closest('.edit-button');
+            if (editButton) {
+                // 1. Dapatkan semua data dari atribut 'data-*' pada tombol
+                const id = editButton.dataset.id;
+                const name = editButton.dataset.name;
+                const sku = editButton.dataset.sku;
+                const category = editButton.dataset.category;
+                const supplier = editButton.dataset.supplier;
+                const purchasePrice = editButton.dataset.purchase_price;
+                const sellingPrice = editButton.dataset.selling_price;
+                const description = editButton.dataset.description;
+
+                // 2. Isi form di dalam modal edit dengan data tersebut
+                document.getElementById('edit-product-id').value = id;
+                document.getElementById('edit-name').value = name;
+                document.getElementById('edit-sku').value = sku;
+                document.getElementById('edit-category').value = category;
+                document.getElementById('edit-supplier').value = supplier;
+                document.getElementById('edit-purchase_price').value = purchasePrice;
+                document.getElementById('edit-selling_price').value = sellingPrice;
+                document.getElementById('edit-description').value = description;
+            }
+        });
+    });
+</script>
+@endpush
