ece8e2e5fedba2de76583d54bf1620638bbeb5c9
Ulul Azmi A. Latala <ullulazmia.l@gmail.com>
2025-10-01 15:05:38 +0700

    Update sidebar

--- resources/views/app/components/sidebar.blade.php
+++ resources/views/app/components/sidebar.blade.php
@@ -17,68 +17,55 @@
                 </a>
             </li>
 
-            {{-- 2. MENU TRANSAKSI (Aksi Harian) --}}
-            @hasanyrole('manager|staff')
-            @php
-                $isTransactionActive = request()->routeIs(['stock.in.create', 'stock.out.create', 'staff.stock.confirm-in', 'staff.stock.prepare-out']);
-            @endphp
-            <li>
-                <button type="button" class="flex items-center w-full p-2 text-base rounded-lg group transition duration-200 ease-in-out hover:translate-x-2 {{ $inactiveClass }} {{ $isTransactionActive ? 'text-primary-600 dark:text-primary-400' : '' }}" aria-controls="dropdown-transactions" data-collapse-toggle="dropdown-transactions">
-                    <i class="w-5 h-5 text-center text-gray-500 fa-solid fa-arrow-right-arrow-left dark:text-gray-400 transition duration-200 ease-in-out group-hover:text-primary-600 dark:group-hover:text-primary-400 {{ $isTransactionActive ? 'text-primary-600 dark:text-primary-400' : '' }}"></i>
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Transaksi</span>
-                    <i class="fa-solid fa-chevron-down"></i>
-                </button>
-                <ul id="dropdown-transactions" class="py-2 space-y-2 {{ $isTransactionActive ? '' : 'hidden' }}">
-                    @role('manager')
-                        <li><a href="{{ route('stock.in.create') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('stock.in.create') ? $activeClass : $inactiveClass }}">Barang Masuk</a></li>
-                        <li><a href="{{ route('stock.out.create') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('stock.out.create') ? $activeClass : $inactiveClass }}">Barang Keluar</a></li>
-                    @endrole
-                    @role('staff')
-                        <li><a href="{{ route('staff.stock.confirm-in') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('staff.stock.confirm-in') ? $activeClass : $inactiveClass }}">Konfirmasi Penerimaan</a></li>
-                        <li><a href="{{ route('staff.stock.prepare-out') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('staff.stock.prepare-out') ? $activeClass : $inactiveClass }}">Siapkan Pengeluaran</a></li>
-                    @endrole
-                </ul>
-            </li>
-            @endhasanyrole
-
-            {{-- 3. MENU STOK (Penyesuaian & Laporan Stok) --}}
-            @hasanyrole('admin|manager')
-             @php
-                $isStockActive = request()->routeIs(['admin.stock.report', 'manager.stock.opname']);
-            @endphp
-            <li>
-                <a href="{{ Auth::user()->hasRole('admin') ? route('admin.stock.report') : route('manager.stock.opname') }}" 
-                   class="flex items-center p-2 rounded-lg group transition duration-200 ease-in-out hover:translate-x-2 {{ $isStockActive ? $activeClass : $inactiveClass }}">
-                    <i class="w-5 h-5 text-center text-gray-500 fa-solid fa-boxes-stacked dark:text-gray-400 transition duration-200 ease-in-out group-hover:text-primary-600 dark:group-hover:text-primary-400 {{ $isStockActive ? 'text-primary-600 dark:text-primary-400' : '' }}"></i>
-                    <span class="ms-3">{{ Auth::user()->hasRole('admin') ? 'Laporan Stok' : 'Stock Opname' }}</span>
-                </a>
-            </li>
-            @endhasanyrole
-
-            {{-- 4. MENU MASTER DATA (Hanya Admin) --}}
+            {{-- 2. MENU MANAJEMEN PRODUK (Hanya Admin) --}}
             @role('admin')
             @php
-                $isMasterDataActive = request()->routeIs(['products.*','categories.*','suppliers.*','attributes.*', 'users.*']);
+                $isProductMenuActive = request()->routeIs(['products.*', 'categories.*', 'suppliers.*', 'attributes.*']);
             @endphp
             <li>
-                <button type="button" class="flex items-center w-full p-2 text-base rounded-lg group transition duration-200 ease-in-out hover:translate-x-2 {{ $inactiveClass }} {{ $isMasterDataActive ? 'text-primary-600 dark:text-primary-400' : '' }}" aria-controls="dropdown-data" data-collapse-toggle="dropdown-data">
-                    <i class="w-5 h-5 text-center text-gray-500 fa-solid fa-database dark:text-gray-400 transition duration-200 ease-in-out group-hover:text-primary-600 dark:group-hover:text-primary-400 {{ $isMasterDataActive ? 'text-primary-600 dark:text-primary-400' : '' }}"></i>
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Master Data</span>
+                <button type="button" class="flex items-center w-full p-2 text-base rounded-lg group transition duration-200 ease-in-out hover:translate-x-2 {{ $inactiveClass }} {{ $isProductMenuActive ? 'text-primary-600 dark:text-primary-400' : '' }}" aria-controls="dropdown-products" data-collapse-toggle="dropdown-products">
+                    <i class="w-5 h-5 text-center text-gray-500 fa-solid fa-box-archive dark:text-gray-400 transition duration-200 ease-in-out group-hover:text-primary-600 dark:group-hover:text-primary-400 {{ $isProductMenuActive ? 'text-primary-600 dark:text-primary-400' : '' }}"></i>
+                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Manajemen Produk</span>
                     <i class="fa-solid fa-chevron-down"></i>
                 </button>
-                <ul id="dropdown-data" class="py-2 space-y-2 {{ $isMasterDataActive ? '' : 'hidden' }}">
-                    <li><a href="{{ route('products.index') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('products.*') ? $activeClass : $inactiveClass }}">Produk</a></li>
+                <ul id="dropdown-products" class="py-2 space-y-2 {{ $isProductMenuActive ? '' : 'hidden' }}">
+                    <li><a href="{{ route('products.index') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('products.*') ? $activeClass : $inactiveClass }}">Daftar Produk</a></li>
                     <li><a href="{{ route('categories.index') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('categories.*') ? $activeClass : $inactiveClass }}">Kategori</a></li>
                     <li><a href="{{ route('suppliers.index') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('suppliers.*') ? $activeClass : $inactiveClass }}">Supplier</a></li>
                     <li><a href="{{ route('attributes.index') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('attributes.*') ? $activeClass : $inactiveClass }}">Atribut</a></li>
-                    <li><a href="{{ route('users.index') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('users.*') ? $activeClass : $inactiveClass }}">Pengguna</a></li>
                 </ul>
             </li>
             @endrole
+            
+            {{-- 3. MENU MANAJEMEN STOK (Semua Role, Tampilan Berbeda) --}}
+            @php
+                $isStockMenuActive = request()->routeIs(['stock.in.create', 'stock.out.create', 'manager.stock.opname', 'staff.stock.confirm-in', 'staff.stock.prepare-out', 'admin.stock.report']);
+            @endphp
+             <li>
+                <button type="button" class="flex items-center w-full p-2 text-base rounded-lg group transition duration-200 ease-in-out hover:translate-x-2 {{ $inactiveClass }} {{ $isStockMenuActive ? 'text-primary-600 dark:text-primary-400' : '' }}" aria-controls="dropdown-stock" data-collapse-toggle="dropdown-stock">
+                    <i class="w-5 h-5 text-center text-gray-500 fa-solid fa-boxes-stacked dark:text-gray-400 transition duration-200 ease-in-out group-hover:text-primary-600 dark:group-hover:text-primary-400 {{ $isStockMenuActive ? 'text-primary-600 dark:text-primary-400' : '' }}"></i>
+                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Manajemen Stok</span>
+                    <i class="fa-solid fa-chevron-down"></i>
+                </button>
+                <ul id="dropdown-stock" class="py-2 space-y-2 {{ $isStockMenuActive ? '' : 'hidden' }}">
+                    @role('admin')
+                        <li><a href="{{ route('admin.stock.report') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('admin.stock.report') ? $activeClass : $inactiveClass }}">Laporan Stok</a></li>
+                    @endrole
+                    @role('manager')
+                        <li><a href="{{ route('stock.in.create') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('stock.in.create') ? $activeClass : $inactiveClass }}">Catat Barang Masuk</a></li>
+                        <li><a href="{{ route('stock.out.create') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('stock.out.create') ? $activeClass : $inactiveClass }}">Catat Barang Keluar</a></li>
+                        <li><a href="{{ route('manager.stock.opname') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('manager.stock.opname') ? $activeClass : $inactiveClass }}">Stock Opname</a></li>
+                    @endrole
+                    @role('staff')
+                        <li><a href="{{ route('staff.stock.confirm-in') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('staff.stock.confirm-in') ? $activeClass : $inactiveClass }}">Konfirmasi Masuk</a></li>
+                        <li><a href="{{ route('staff.stock.prepare-out') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('staff.stock.prepare-out') ? $activeClass : $inactiveClass }}">Siapkan Keluar</a></li>
+                    @endrole
+                </ul>
+            </li>
 
-            {{-- 5. MENU LAPORAN (Hanya Admin & Manajer) --}}
+            {{-- 4. MENU LAPORAN --}}
             @hasanyrole('admin|manager')
-             @php
+            @php
                 $isReportActive = request()->routeIs(['transactions.index', 'admin.reports.activity-log']);
             @endphp
             <li>
@@ -89,14 +76,25 @@
                 </button>
                 <ul id="dropdown-reports" class="py-2 space-y-2 {{ $isReportActive ? '' : 'hidden' }}">
                     <li><a href="{{ route('transactions.index') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('transactions.index') ? $activeClass : $inactiveClass }}">Riwayat Transaksi</a></li>
-                     @role('admin')
-                    <li><a href="{{ route('admin.reports.activity-log') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('admin.reports.activity-log') ? $activeClass : $inactiveClass }}">Aktivitas Pengguna</a></li>
-                     @endrole
+                    @role('admin')
+                        <li><a href="{{ route('admin.reports.activity-log') }}" class="flex items-center w-full p-2 rounded-lg pl-11 group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('admin.reports.activity-log') ? $activeClass : $inactiveClass }}">Aktivitas Pengguna</a></li>
+                    @endrole
                 </ul>
             </li>
             @endhasanyrole
+
+            {{-- 5. MENU MANAJEMEN PENGGUNA --}}
+            @role('admin')
+            <li>
+                <a href="{{ route('users.index') }}" 
+                   class="flex items-center p-2 rounded-lg group transition duration-200 ease-in-out hover:translate-x-2 {{ request()->routeIs('users.*') ? $activeClass : $inactiveClass }}">
+                    <i class="w-5 h-5 text-center text-gray-500 fa-solid fa-users dark:text-gray-400 transition duration-200 ease-in-out group-hover:text-primary-600 dark:group-hover:text-primary-400 {{ request()->routeIs('users.*') ? 'text-primary-600 dark:text-primary-400' : '' }}"></i>
+                    <span class="ms-3">Manajemen Pengguna</span>
+                </a>
+            </li>
+            @endrole
             
-            {{-- 6. MENU PENGATURAN (Hanya Admin) --}}
+            {{-- 6. MENU PENGATURAN --}}
             @role('admin')
             <li>
                 <a href="{{ route('settings.index') }}" 
